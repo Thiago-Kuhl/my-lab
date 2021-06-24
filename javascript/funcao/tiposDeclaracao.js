87d3ab5054988e5abf3e191d1a1b30316b7a5467
Thiago Kühl <thiago.kuhl15@gmail.com>
2021-06-23 21:56:42 -0300

    more review

---
+++ javascript/funcao/tiposDeclaracao.js
@@ -0,0 +1,17 @@
+console.log(soma(3, 4))
+//function declaration
+function soma(a, b){
+    return a + b
+}
+
+//function expression
+const sub = function (a, b){
+    return a - b
+}
+
+//named funciton expression
+const mult = function mult(a, b){
+    return a * b
+}
+
+//
